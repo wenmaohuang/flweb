2755b8bf31c81478980cf0eb555d782d6e020fb0
风移影动 <oncwnuFE28RNcGrpLh3eWbhn5qM4@git.weixin.qq.com>
2020-10-26 02:06:01 +0800

    '1'

--- server/app.js
+++ server/app.js
@@ -3,33 +3,13 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
-
-
-
 var mongoose = require('mongoose')
-
 var app = express();
-
-
-
-
-
-// mongoose.connect("mongodb://localhost:27017/blog",{ useNewUrlParser: true, useUnifiedTopology: true })
-//     .then(()=>{console.log("数据库连接成功")})
-//     .catch(()=>{console.log("数据库连接失败")});
-
-
-// view engine setup
-
-
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../build/web')));
-// app.use(express.static(path.join(__dirname, '../build/web')));
 app.use(express.static(path.join(__dirname, 'public')));
 app.get('*', function (request, response){
     response.sendFile(path.resolve(__dirname, '../build/web', 'index.html'))
@@ -54,38 +34,4 @@ app.use((req, res, next) => {
 
 app.use(require('./session/index.js'))
 app.use('/', require('./routes/index.js'))
-
-// const routesList = [
-//     {
-//         path: '/',
-//         name: 'admin',
-//     },
-//     {
-//         path: '',
-//         name: 'adminIndex',
-//     },
-//     {
-//         path: '/articleAdd',
-//                 name: 'Add',
-//     },
-//     {
-//         path: '/articleManager',
-//                 name: 'Manager',
-//     },
-//     {
-//         path: '/login',
-//         name: 'login',
-//     }
-// ]
-// routesList.forEach(item=>{
-// app.get(item.path,(req,res)=>{
-//     res.sendFile(require('path').join(__dirname,'../client/dist/index.html'))
-// })
-// })
-
-
-
-
-
-
 module.exports = app;
